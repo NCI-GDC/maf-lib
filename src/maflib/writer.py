9c8162825ed69b1ee4c96fdd374e854698b8a8cb
Nils Homer <nilshomer@gmail.com>
2017-08-25 14:59:21 -0700

    feat(sorting): check sorting when writing
    Sorting will occur if it is not assumed to be sorted already, or the
    sort order is unknown or unsorted.

--- src/maflib/writer.py
+++ src/maflib/writer.py
@@ -8,16 +8,22 @@ import gzip
 
 from maflib.logger import Logger
 from maflib.record import MafRecord
+from maflib.schemes import NoRestrictionsScheme
+from maflib.sort_order import SortOrderChecker
+from maflib.sorter import MafSorter
 from maflib.validation import ValidationStringency
 
 
 class MafWriter(object):
     """A writer of a MAF file"""
 
-    def __init__(self, handle, header, validation_stringency=None):
+    def __init__(self, handle, header, validation_stringency=None, assume_sorted=True):
         self._handle = handle
         self._header = header
         self._logger = Logger.get_logger(self.__class__.__name__)
+        self._assume_sorted = assume_sorted
+        self._sorter = None
+        self._checker = None
 
         self.validation_stringency = ValidationStringency.Silent \
             if (validation_stringency is None) else validation_stringency
@@ -32,14 +38,27 @@ class MafWriter(object):
         if len(self._header) > 0:
             self._handle.write(str(self._header) + "\n")
 
-        scheme = self._header.scheme()
-        if scheme:
+
+        # write the column names if we have a scheme
+        self._scheme = self._header.scheme()
+        if self._scheme:
             self._handle.write(
-                MafRecord.ColumnSeparator.join(scheme.column_names()) + "\n"
-            )
-            self._written_column_names = True
+                MafRecord.ColumnSeparator.join(self._scheme.column_names())
+                + "\n")
+            self._set_checker_and_sorter()
+
+    def _set_checker_and_sorter(self):
+        """Set the sort order checker and sorter.  Must be called **after** 
+        the scheme has been set."""
+        if self._assume_sorted or not self._header.sort_order().sort_key():
+            self._checker = SortOrderChecker(self._header.sort_order())
+            self._sorter = None
         else:
-            self._written_column_names = False
+            self._checker = None
+            self._sorter = MafSorter(
+                sort_order_name=self._header.sort_order().name(),
+                scheme=self._scheme
+            )
 
     def header(self):
         """Get the underlying MafHeader. """
@@ -48,25 +67,29 @@ class MafWriter(object):
     def __iadd__(self, record):
         """Write a MafRecord. """
 
+        # set the scheme and write the column names if not already written
+        if not self._scheme:
+            column_names = [str(key) for key in record.keys()]
+            self._scheme = NoRestrictionsScheme(column_names=column_names)
+            self._handle.write(
+                MafRecord.ColumnSeparator.join(self._scheme.column_names())
+                + "\n")
+            self._set_checker_and_sorter()
+
         # validate the record
-        scheme = self._header.scheme()
         record.validate(
             validation_stringency=self.validation_stringency,
             logger=self._logger,
             reset_errors=True,
-            scheme=scheme
+            scheme=self._scheme
         )
 
-        # write the column names if not already written
-        if not self._written_column_names:
-            line = MafRecord.ColumnSeparator.join(
-                str(key) for key in record.keys()
-            )
-            self._handle.write(line + "\n")
-            self._written_column_names = True
+        # either write it directly, or add it to the sorter
+        if self._sorter:
+            self._sorter += record
+        else:
+            self._handle.write(str(record) + "\n")
 
-        # write it
-        self._handle.write(str(record) + "\n")
         return self
 
     def write(self, record):
@@ -74,26 +97,34 @@ class MafWriter(object):
         return self.__iadd__(record)
 
     def close(self):
-        """Closes the underlying file handle"""
+        """Closes the underlying file handle, and writes the records if the 
+        output was to be sorted."""
+        if self._sorter:
+            for rec in self._sorter:
+                self._handle.write(str(rec) + "\n")
+            self._sorter.close()
         self._handle.close()
 
     @classmethod
     def from_fd(cls,
                 desc,
                 header,
-                validation_stringency=None):
+                validation_stringency=None,
+                assume_sorted=True):
         """Create a MafWriter from the given file handle."""
         return MafWriter(
             handle=desc,
             header=header,
-            validation_stringency=validation_stringency
+            validation_stringency=validation_stringency,
+            assume_sorted=assume_sorted
         )
 
     @classmethod
     def from_path(cls,
                   path,
                   header,
-                  validation_stringency=None):
+                  validation_stringency=None,
+                  assume_sorted=True):
         """Create a MafWriter from the given path ."""
         if path.endswith(".gz"):
             handle = gzip.open(path, "wt")
@@ -102,5 +133,6 @@ class MafWriter(object):
         return MafWriter.from_fd(
             desc=handle,
             header=header,
-            validation_stringency=validation_stringency
+            validation_stringency=validation_stringency,
+            assume_sorted=assume_sorted
         )
