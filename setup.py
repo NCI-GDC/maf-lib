8e59cb9402c6da2428084aadefb8c747b78d543f
Charles Czysz <czysz@uchicago.edu>
2021-05-20 08:49:35 -0500

    Linting

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ GIT_REPO_URL = "https://github.com/NCI-GDC/{}".format(GIT_REPO)
 
 setup(
     url=GIT_REPO_URL,
-    use_scm_version={"relative_to": __file__, 'write_to': f'{PACKAGE}/_version.py',},
+    use_scm_version={"relative_to": __file__, 'write_to': f'{PACKAGE}/_version.py'},
     setup_requires=['setuptools_scm'],
 )
 
