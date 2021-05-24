bfbd648ac6f93685852b8bf6c3609f2171cdc28d
Charles Czysz <czysz@uchicago.edu>
2021-05-24 08:35:40 -0500

    Move more out of setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,8 @@
 
 from setuptools import setup
 
-GIT_REPO = "maf-lib"
-PACKAGE = "maflib"
-
-GIT_REPO_URL = "https://github.com/NCI-GDC/{}".format(GIT_REPO)
-
-
 setup(
-    url=GIT_REPO_URL,
-    setup_requires=['setuptools_scm'],
-    use_scm_version={"relative_to": __file__, 'write_to': f'{PACKAGE}/_version.py'},
+    use_scm_version=True, setup_requires=['setuptools_scm'],
 )
 
 # __END__
