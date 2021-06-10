e54e7e8e1717563a8854f1f8f717ead679d60469
Charles Czysz <czysz@uchicago.edu>
2021-06-10 11:35:56 -0500

    Update with type hints

--- maflib/locatable.py
+++ maflib/locatable.py
@@ -1,60 +1,65 @@
 """A module for locatable records."""
 
 
-class Locatable(object):
+from typing import List
+
+
+class Locatable:
     """A class that defines a genomic location (or span)."""
 
-    def __init__(self, chromosome, start, end):
+    def __init__(self, chromosome: str, start: int, end: int):
         self._chromosome = chromosome
         self._start = start
         self._end = end
 
     @property
-    def chromosome(self):
+    def chromosome(self) -> str:
         """Returns the chromosome name"""
         return self._chromosome
 
     @chromosome.setter
-    def chromosome(self, value):
+    def chromosome(self, value: str) -> None:
         """Sets the chromosome position"""
         self._chromosome = value
 
     @property
-    def start(self):
+    def start(self) -> int:
         """Returns the start position"""
         return self._start
 
     @start.setter
-    def start(self, value):
+    def start(self, value: int) -> None:
         """Sets the start position"""
         self._start = value
 
     @property
-    def end(self):
+    def end(self) -> int:
         """Returns the end position"""
         return self._end
 
     @end.setter
-    def end(self, value):
+    def end(self, value: int) -> None:
         """Sets the end position"""
         self._end = value
 
 
 class LocatableByAllele(Locatable):
-    """A class that defines a genomic location (or span), with an associated
-    reference and set of alternate alleles (may be empty)"""
+    """Defines a genomic location (or span) with ref and alt alleles.
+    """
 
-    def __init__(self, chromosome, start, end, ref, alts):
+    def __init__(
+        self, chromosome: str, start: int, end: int, ref: str, alts: List[str]
+    ):
         self._ref = ref
         self._alts = alts
         super(LocatableByAllele, self).__init__(chromosome, start, end)
 
     @property
-    def ref(self):
+    def ref(self) -> str:
         """Returns the reference allele"""
         return self._ref
 
     @property
-    def alts(self):
+    def alts(self) -> List[str]:
         """Returns a list of valid alternate alleles"""
         return self._alts
